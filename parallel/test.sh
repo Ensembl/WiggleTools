80f19d1edd74c54b052d884da21c3c4c0d8c367b
Daniel Zerbino <zerbino@ebi.ac.uk>
2013-11-10 22:58:04 +0000

    Removing old tests

--- parallel/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-parallelWiggleTools.py /lustre/scratch109/ensembl/dz1/bigwigs/chrom_lengths 'write /lustre/scratch109/ensembl/dz1/TATA.bw add /lustre/scratch109/ensembl/dz1/bigwigs/wgEncodeBroadHistoneHepg2ControlStdAln_2Reps.norm5.rawsignal.bw'
