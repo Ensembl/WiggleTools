6e3fd8bc64a78f363e433d9d0e781818ec947a3e
Daniel Zerbino <zerbino@ebi.ac.uk>
2014-02-11 10:28:07 +0000

    Warning in BedReader

--- src/bedReader.c
+++ src/bedReader.c
@@ -56,7 +56,7 @@ void BedReaderPop(WiggleIterator * wi) {
 		}
 
 		if (data->stop > 0) {
-			if (wi->start >= data->stop && strcmp(wi->chrom, data->chrom) == 0 || strcmp(wi->chrom, data->chrom) > 0) {
+			if ((wi->start >= data->stop && strcmp(wi->chrom, data->chrom) == 0) || strcmp(wi->chrom, data->chrom) > 0) {
 				wi->done = true;
 				return;
 			} else if (wi->finish > data->stop) {
