1f4004419b4af416b478f45e690c0a3aad83b438
Daniel Zerbino <zerbino@ebi.ac.uk>
2014-06-17 09:57:48 +0000

    Adding S3 instructions

--- python/wiggletools/wiggleDB_finish.py
+++ python/wiggletools/wiggleDB_finish.py
@@ -4,6 +4,8 @@ import sys
 import argparse
 import subprocess
 import glob
+import os
+import os.path
 
 import wiggleDB
 import multiJob 
@@ -18,24 +20,30 @@ def get_options():
 	parser.add_argument('--apply_paste','-v',dest='apply_paste', help='Run Apply_paste command')
 	parser.add_argument('--labels','-l',dest='labels', help='Labels for plot categories', nargs='*')
 	parser.add_argument('--email','-e',dest='emails', help='Notification e-mail addresses', nargs='*')
+	parser.add_argument('--s3',dest='s3', help='S3 bucket')
 	return parser.parse_args()
 
 def main():
 	options = get_options()
+	data = wiggleDB.get_job_location(options.db_name, options.jobID)
 	if options.temps is not None:
 		multiJob.clean_temp_files(options.temps)
 	if options.histogram is not None:
 		if subprocess.call("wiggletools "  + options.histogram, shell=True):
 			print "Failed to construct histogram"
 			sys.exit(1)
-		data = wiggleDB.get_job_location(options.db_name, options.jobID)
 		wigglePlots.make_histogram(data, options.labels, data + ".pdf")
 	if options.apply_paste is not None:
 		if subprocess.call("wiggletools "  + options.apply_paste, shell=True):
 			print "Failed to construct overlap graph"
 			sys.exit(1)
-		data = wiggleDB.get_job_location(options.db_name, options.jobID)
 		wigglePlots.make_overlaps(data, data + ".pdf")
+	if options.s3 is not None:
+		cmd = "aws s3 cp %s s3://%s/%s --acl public-read" % (data, options.s3, os.path.basename(data))
+		if subprocess.call(cmd, shell=True):
+			print "Failed to copy over results"
+			print cmd
+			sys.exit(1)
 	wiggleDB.mark_job_as_done(options.db_name, options.jobID)
 
 if __name__ == "__main__":
