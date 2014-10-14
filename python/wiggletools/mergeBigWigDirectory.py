fc16c3fb300f8f6a072bea6332dd4bdf97202300
Daniel Zerbino <zerbino@ebi.ac.uk>
2014-10-14 16:01:52 +0000

    Added handling of empty results

--- python/wiggletools/mergeBigWigDirectory.py
+++ python/wiggletools/mergeBigWigDirectory.py
@@ -7,20 +7,24 @@ import shutil
 import os
 
 directory = sys.argv[1]
+bigwigs = glob.glob(os.path.join(directory + "x", '*.bw'))
 
-command = ['catBigWigs', directory] + glob.glob(os.path.join(directory + "x", '*.bw'))
+if len(bigwigs) > 0:
+	command = ['catBigWigs', directory] + bigwigs
+	p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	return_code = p.wait()
 
-p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	if return_code != 0:
+		out, err = p.communicate()
+		print "Failed: %s" % " ".join(command)
+		print "Stdout:"
+		print out
+		print "Stderr:"
+		print err
+		sys.exit(1)
 
-return_code = p.wait()
-
-if return_code != 0:
-	out, err = p.communicate()
-	print "Failed: %s" % " ".join(command)
-	print "Stdout:"
-	print out
-	print "Stderr:"
-	print err
-	sys.exit(1)
+else:
+	# Create empty file with .empty suffix
+	open(directory + ".empty", "w").close()
 
 shutil.rmtree(directory + "x")
--- python/wiggletools/mergeProfilesDirectory.py
+++ python/wiggletools/mergeProfilesDirectory.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import wigglePlots
 import os
+import os.path
 import shutil
 
 file=sys.argv[1]
@@ -11,4 +12,9 @@ if (subprocess.call("sort -k1,1 -k2,2n -k3,3n -m %sx/* > %s" % (file, file), she
 	print 'Error processing directory %sx' % file
 	sys.exit(1)
 shutil.rmtree(file + "x")
-wigglePlots.make_profiles_matrix(file, file + ".pdf")
+
+if os.path.getsize(file) > 0:
+	wigglePlots.make_profiles_matrix(file, file + ".pdf")
+else:
+	# Create empty file with .empty suffix
+	open(file + ".empty", "w").close()
